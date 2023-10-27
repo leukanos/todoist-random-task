0be6b7ebad7f1fdcf3319a270dea16636a2a59d4
leukanos <kontakt@lostrowski.pl>
2023-10-27 09:16:41 +0000

    Update handler

--- handler.ts
+++ handler.ts
@@ -1,35 +1,25 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { TodoistApi } from '@doist/todoist-api-typescript'
 
-const token = process.env.TODOIST_SECRET_TOKEN;
+const token = process.env.TODOIST_SECRET_TOKEN as string;
 const api = new TodoistApi(token);
 
 interface Task {
   id: string;
   content: string;
-  description: string; 
+  description: string;
 }
-
-const tasks: Task[] = [
-  {
-    id: "1",
-    content: "asdf"
-  }
-]
 
 export const hello = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  console.log(tasks[0]);
+
+  const tasks = await getTasks();
 
   return {
     statusCode: 200,
     body: JSON.stringify(
-{
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        token: token, 
-        input: event,
-      },
+      getRandom(tasks),
       null,
       2
     ),
@@ -39,5 +29,11 @@ export const hello = async (
 const getRandom = (tasks: Array<Task>): Task => {
   return tasks[
     Math.floor(Math.random() * tasks.length)
-  ]  
+  ]
 }
+
+const getTasks = async (): Promise<Task[]> => {
+  const tasks = await api.getTasks({"label": "random"});
+  return tasks;
+}
+
